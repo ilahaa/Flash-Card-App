988db8b8d871be80205875dd37f6b11da52a2ce2
Ilaha Hasanli <hasanliilaha12@gmail.com>
2023-12-30 23:22:40 +0400

    add flash cards

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import bookData from "./data/mydata.json"
+import React, { useState } from 'react';
+import bookData from './data/mydata.json';
+import '../src/style/flashCard.css';
 
 const FlashCard = () => {
-  return (
-    <div>
-      <h1>Favorite Books</h1>
-      {bookData.cards.map((book) => (
-        <div key={book.id}>
-          <h2>{book.name}</h2>
-          <p>Author: {book.author}</p>
-          <img style={{width: 100}} src={book.img} alt={book.name} />
-          <p>Pages: {book.pages}</p>
-          <p>Date Published: {book.datePublished}</p>
-          <p>Genres: {book.genres}</p>
-          <p>Description: {book.desc}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+  const [selectedBook, setSelectedBook] = useState(null);
 
-export default FlashCard
+  const handleCardClick = (bookId) => {
+    setSelectedBook(bookId === selectedBook ? null : bookId);
+  };
+
+  return (
+    <div className="flashCardPage m-5">
+      <h1>Favorite Books</h1>
+      <div className="books-container">
+        {bookData.cards.map((book) => (
+          <div
+            key={book.id}
+            className={`cardItem ${selectedBook === book.id ? 'is-flipped' : ''}`}
+            onClick={() => handleCardClick(book.id)}
+          >
+            <div className="card front">
+              <img src={book.img} alt={book.name} />
+            </div>
+            <div className="card back">
+              <h3>{book.name}</h3>
+              <p>Author: {book.author}</p>
+              <p>{book.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FlashCard;
